553de7c8e555ff1f833692f58d9dc7f25a190169
wuchenru <chenruwu31@gmail.com>
2025-10-09 16:31:25 -0700

    set response

--- app/main.py
+++ app/main.py
@@ -1,25 +1,21 @@
-import socket  # noqa: F401
-
+import socket
 
 def main():
-    # You can use print statements as follows for debugging,
-    # they'll be visible when running tests.
     print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    #
+    
     server = socket.create_server(("localhost", 9092), reuse_port=True)
-    server.accept() # wait for client
+    
+    # 只 accept 一次
     conn, addr = server.accept()
     
-    data = conn.recv(1024)  
+    # 读取请求内容（不解析即可）
+    _ = conn.recv(1024)
     
+    # 构造响应：message_size + correlation_id
     response = b'\x00\x00\x00\x00' + b'\x00\x00\x00\x07'
-    
     conn.sendall(response)
     
-    
-
+    conn.close()
 
 if __name__ == "__main__":
     main()
